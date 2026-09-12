24a6ce0a6b89d2fb26c9bb15c2f10ebe2ebf146e
arvinloc <sulvinskijartiom@gmail.com>
2026-09-12 07:43:39 -0700

    01-03 solved

---
+++ src/by/it/group551051/shulvinski/lesson01/FiboC.java
@@ -0,0 +1,67 @@
+package by.it.group551051.shulvinski.lesson01;
+
+/*
+ * Даны целые числа 1<=n<=1E18 и 2<=m<=1E5,
+ * необходимо найти остаток от деления n-го числа Фибоначчи на m
+ * время расчета должно быть не более 2 секунд
+ */
+
+import java.math.BigInteger;
+
+public class FiboC {
+
+    private long startTime = System.currentTimeMillis();
+
+    public static void main(String[] args) {
+        FiboC fibo = new FiboC();
+        int n = 55555;
+        int m = 1000;
+        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
+    }
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    // Для решения этой задачи необходимо использовать период Пизано
+
+    long getPisanoPeriod(int m){
+        long prev = 0;
+        long curr = 1;
+
+        for(int i = 0; i < 6L * m; i++){
+            long t = curr;
+            curr = (curr + prev) % m;
+            prev = t;
+
+            if (prev == 0 && curr == 1){
+                return i + 1;
+            }
+        }
+        return 0;
+    }
+    long fasterC(long n, int m) {
+        //Интуитивно найти решение не всегда просто и
+        //возможно потребуется дополнительный поиск информации
+
+        long pisanoPeriod = getPisanoPeriod(m);
+
+        n = n % pisanoPeriod;
+
+        if (n < 2){
+            return n;
+        }
+        long prev = 0;
+        long curr = 1;
+
+        for (long i = 0; i < n - 1; i++){
+            long t = curr;
+            curr = (curr + prev) % m;
+            prev = t;
+        }
+        return curr;
+    }
+
+
+}
+
